5e8ee7f6993262e5438ab83b7817d0d5af907b01
Joseph Friend <jt2friend@gmail.com>
2017-06-08 00:17:10 +0100

    First commit, starting graphic part

---
+++ SpriteGame/SpriteGame/SpriteGame.cpp
@@ -0,0 +1,16 @@
+// SpriteGame.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "Graphics.h"
+
+int main()
+{
+	Graphics Graphics1;
+	while (true)
+	{
+
+	}
+    return 0;
+}
+
